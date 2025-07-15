ebaced0cbab0c94cb479dbe2b5054a5b19772d3e
Mario Salvador <marioelsalva@gmail.com>
2025-07-15 18:09:00 +0200

    F2: Load todo items

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,31 @@
 import { Container } from "./components/Container";
+import { Footer } from "./components/Footer";
+import { Header } from "./components/Header";
 import { Layout } from "./components/Layout";
 import { List } from "./components/List";
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import './global.css';
-
-export const App = () => (
+import { ListItem } from "./components/ListItem";
+import "./global.css";
+import { Item, useItems } from "./hooks/useItems";
+export const App = () => {
+    const { data, isLoading, error } = useItems();
+    return (
         <Container>
             <Layout>
                 <Header onItemAdd={() => console.warn("unimplemented")}>To Do app</Header>
-                <List />
-                <Footer />
+                <List>
+                    {data?.map((item: Item) => (
+                        <ListItem
+                            key={item.id}
+                            label={item.label}
+                            isDone={item.isDone}
+                            onItemLabelEdit={() => console.warn("unimplemented")}
+                            onItemDoneToggle={() => console.warn("unimplemented")}
+                            onItemDelete={() => console.warn("unimplemented")}
+                        />
+                    ))}
+                </List>
+                <Footer todoItems={data?.length} doneItems={data?.filter((item: Item) => item.isDone).length} />
             </Layout>
         </Container>
-);
+    );
+};
